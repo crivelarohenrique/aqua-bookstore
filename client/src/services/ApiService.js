4576d581df0e5633c409a81be5e2282cc394f481
Henrique Crivelaro <142052033+crivelarohenrique@users.noreply.github.com>
2024-08-21 14:52:48 -0300

    Update ApiService.js

--- client/src/services/ApiService.js
+++ client/src/services/ApiService.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: "https://react-library-api.vercel.app",
+    baseURL: "https://react-library-frontend-flame.vercel.app/api",
     withCredentials: true,
     headers: {
         'Content-Type': 'application/json',
